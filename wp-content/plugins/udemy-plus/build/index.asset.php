f754314c82f49c5c1555e4d87f4d7bc79faa57da
Dilan Garcia <dilanngcia@gmail.com>
2022-07-27 14:28:43 -0600

    block registered

--- wp-content/plugins/udemy-plus/build/index.asset.php
+++ wp-content/plugins/udemy-plus/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks'), 'version' => '64fb0aef5f103f04e8e4');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'e92794858184188444bc');
--- wp-content/plugins/udemy-plus/src/index.js
+++ wp-content/plugins/udemy-plus/src/index.js
@@ -1,1 +1,28 @@
-import registerBlockType from '@wordpress/blocks';
+import { registerBlockType } from '@wordpress/blocks'
+import { RichText } from '@wordpress/block-editor'
+import { __ } from '@wordpress/i18n'
+import block from './block.json'
+
+registerBlockType(block.name, {
+  edit({ attributes, setAttributes }) {
+    const {content} = attributes
+
+    return (
+      <RichText tagName="h2" 
+      placeholder={__('Enter Heading', 'udemy-plus')}
+      value={content}
+      onChange={newVal => setAttributes({content: newVal})}
+      />
+    )
+  },
+  save({ attributes }) {
+    const {content} = attributes
+
+    return (
+      <RichText.Content
+        tagName="h2"
+        value={content}
+    />)
+  }
+})
+console.log('hola');
