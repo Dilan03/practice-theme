30fc094ce7f0e31d5fdd6da4c6454d3405661a84
Dilan Garcia <dilanngcia@gmail.com>
2022-07-27 11:37:10 -0600

    translation update

---
+++ wp-content/plugins/udemy-plus/index.php
@@ -0,0 +1,18 @@
+<?php 
+/**
+ * Plugin Name:       Udemy plus
+ * Plugin URI:        https://example.com/plugins/the-basics/
+ * Description:       A plugin for adding blocks to a theme
+ * Version:           1.0.0
+ * Requires at least: 5.9
+ * Requires PHP:      7.2
+ * Author:            Dilan
+ * Author URI:        https://author.example.com/
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       udemy-plus
+ * Domain Path:       /languages
+ */
+
+ if(!function_exists('add_action')) {
+  exit;
+ }
